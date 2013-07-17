e69a0249b9d5d9afb4fefc037c715e9ed14a410a
code4bones <clinch.coffin@gmail.com>
2013-07-17 17:15:41 +0400

    Дублирование проекта, исправил..

--- EyeInside/src/com/code4bones/EyeInside/ICommandObj.java
+++
@@ -1,9 +0,0 @@
-package com.code4bones.EyeInside;
-
-public interface ICommandObj {
-
-	
-	public int Invoke() throws Exception;
-	public void Reply(Object ... argv) throws Exception;
-	
-}
--- EyeInside/src/com/code4bones/EyeInside/ICommandObjPlugin.java
+++
@@ -1,9 +0,0 @@
-package com.code4bones.EyeInside;
-
-public interface ICommandObjPlugin {
-	
-	public CommandObj getPlugin();
-	public int Invoke() throws Exception;
-	public void Reply(Object ... argv) throws Exception;
-
-}
